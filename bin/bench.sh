ec30a7988cf4e659329a5e4c57115d21a4fabdf8
Benoit Delbosc <bdelbosc@nuxeo.com>
2019-09-12 17:08:08 +0200

    Move simulation to its own module

--- bin/bench.sh
+++ bin/bench.sh
@@ -3,7 +3,7 @@ ROOT_PATH="$(cd "$(dirname "$0")/.."; pwd -P)"
 MAVEN_OPTS='-Xmx2g -Xms2g -XX:+TieredCompilation -XX:TieredStopAtLevel=1'
 set -e
 set -x
-cd $ROOT_PATH/acme-rest
+cd $ROOT_PATH/acme-simulation
 mvn -nsu test gatling:test -Pbench -DnbMessages=10000
 { set +x; } 2>/dev/null
 echo "### -------------------------------------------------------------------"
